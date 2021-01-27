6840a2a9bb0824d7b0e4e746f3e21b20b8a171bb
6592sho <wxwxwx159@yahoo.co.jp>
2021-01-27 22:42:45 +0900

    [create] とらハックReact入門 実践編

---
+++ react/chat-bot-app/src/components/Chat.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemAvatar from "@material-ui/core/ListItemAvatar";
+import Avatar from "@material-ui/core/Avatar";
+import noProfile from "../assets/img/noProfile.png";
+import tabimaru from "../assets/img/tabimaru.png";
+
+const Chat = (props) => {
+  const isQuestion = props.type === "question";
+  const classes = isQuestion ? "p-chat__row" : "p-chat__reverse";
+  return (
+    <ListItem className={classes}>
+      <ListItemAvatar>
+        {isQuestion ? (
+          <Avatar alt="Icon" src={tabimaru} />
+        ) : (
+          <Avatar alt="Icon" src={noProfile} />
+        )}
+      </ListItemAvatar>
+      <div className={"p-chat__bubble"}>{props.text}</div>
+    </ListItem>
+  );
+};
+
+export default Chat;
---
+++ react/chat-bot-app/src/firebase/config.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCckBmLkvakFdK2nGSI6LE5rw1cz4cHcBw",
+  authDomain: "chatbot-demo-40ffb.firebaseapp.com",
+  projectId: "chatbot-demo-40ffb",
+  storageBucket: "chatbot-demo-40ffb.appspot.com",
+  messagingSenderId: "106648843761",
+  appId: "1:106648843761:web:6046eb1aed51b763a14050",
+  measurementId: "G-T97XVMPZ5P",
+};
+
+export default firebaseConfig;
